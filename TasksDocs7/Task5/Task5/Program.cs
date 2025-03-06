2567fa23f58d2a9aee0b928421a6da0af75cd026
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-06 22:52:12 +0400

    Add files via upload

---
+++ TasksDocs7/Task5/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Task 5:  Multi - Parameter Indexer for 3D Space
+Task: Create a Grid3D class that :
+    1) Stores values in a 3D space (x, y, z coordinates).
+    2) Uses an indexer with three integer parameters.
+    3) Throws an error if any index is out of bounds.
+*/
+
+class Grid3D
+{
+    int[]? xyz = new int[3];
+    public int this[int x, int y, int z]
+    {
+        get
+        {
+            return (x == xyz[0]) && (y == xyz[1]) && (z == xyz[2]) ? 1 : -1;
+        }
+    }
+    public Grid3D(int x, int y, int z)
+    {
+        xyz[0] = x;
+        xyz[1] = y;
+        xyz[2] = z;
+    }
+}
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Grid3D coordinate = new Grid3D(1,2,3);
+        Console.WriteLine(coordinate[2,2,3]);
+        Console.ReadLine();
+    }
+}
