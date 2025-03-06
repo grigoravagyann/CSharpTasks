68e48c7f2b02a9010fe988f661e888b2c7ec9808
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-06 22:51:30 +0400

    Add files via upload

---
+++ TasksDocs7/Task4/Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Task 4:  Student Grades
+Task: Create a Students class that stores grades:
+    1) Implement an indexer where:
+        The key is a subject name.
+        The value is a grade.
+        If the subject doesn’t exist, return -1.
+    2) Use only arrays.
+*/
+
+class Students
+{
+    int[]? _studentsGrades;
+    string[]? _subjectsNames;
+    public int this[string name]
+    {
+        get
+        {
+            for(int i=0; i < _subjectsNames!.Length; i++)
+            {
+                if (_subjectsNames[i] == name)
+                {
+                    return _studentsGrades![i];
+                }
+            }
+            return -1;
+        }
+    }
+    public Students(string[] Subjects, int[] Grades)
+    {
+        _studentsGrades = Grades;
+        _subjectsNames = Subjects;
+    }
+}
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string[] subjects = { "Math", "Physics", "Chemistry", "Biology" };
+        int[] grades = { 90, 80, 70, 60 };
+        Students student = new Students(subjects, grades);
+        Console.WriteLine(student["Math"]);
+        Console.WriteLine(student["Physics"]);
+        Console.WriteLine(student["Chemistry"]);
+        Console.WriteLine(student["Biology"]);
+        Console.WriteLine(student["English"]);
+        Console.Read();
+    }
+}
