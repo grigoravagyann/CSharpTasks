40146d224c5eec2418a9d6057fe3ad6d16eb818c
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-18 15:31:47 +0400

    .

---
+++ ChildrenAndParents/ParentAndChild/ParentClass.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ChildrenAndParents
+{
+    class Parent
+    {
+        string? _parentName;
+        double _parentAge;
+        double _parentSalary;
+        public string Name { get => _parentName!; }
+        public double Age { get => _parentAge; }
+        public double Salary { get => _parentSalary; }
+        public Parent() { }
+        public Parent(string? Name, double Age, double Salary)
+        {
+            _parentName = Name;
+            _parentAge = Age;
+            _parentSalary = Salary;
+        }
+    }
+}
---
+++ ChildrenAndParents/ParentAndChild/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Create Parent and Child classes, the first of which contains information about the parent, the second - about the child.
+Create a static array of Children consisting of 10 elements
+    1) Print a list of children whose father and mother's combined age does not exceed 70.
+    2) Find the salary of the father of the oldest child
+    3) Find the data of the child with the highest family income
+    4) Swap the positions of the youngest and oldest children in the array, printing the total array.
+*/
+
+using ChildrenAndParents;
+
+class Program
+{
+    static void Main()
+    {
+        Child[] children = new Child[10];
+        for (int i = 0; i < children.Length; ++i)
+        {
+            children[i] = new Child($"Child {i}", i, new Parent($"Father {i}", i + 30, i * 1000), new Parent($"Mother {i}", i + 25, i * 1000));
+        }
+        ChildrenMethods.PrintByParentsAge(70, children);
+        ChildrenMethods.MaxAgeFatherSalary(children);
+        ChildrenMethods.MaxFamilyIncomeChild(children);
+        ChildrenMethods.SwapMinMaxAge(children);
+    }
+}
