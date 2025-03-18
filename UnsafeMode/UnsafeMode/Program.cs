98898dcd9df6c05d4d5c594980c65d7a101f19bd
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-18 19:39:05 +0400

    .

---
+++ UnsafeMode/UnsafeMode/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{ 
+    unsafe public static void Main()
+    {
+        int* intArray = stackalloc int[10];
+        Console.WriteLine("Enter 10 numbers: ");
+        for (int i = 0; i < 10; ++i)
+        {
+            intArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int minIndex = 0;
+        for (int i = 1; i < 10; ++i)
+        {
+            if (intArray[i] < intArray[minIndex])
+                minIndex = i;
+        }
+        Console.WriteLine("The smallest number index is: " + minIndex);
+    }
+}
