5dd0d79aef67bbf68c278e6ef2a0e77c82e9a404
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-18 17:22:52 +0400

    .

---
+++ ChildrenAndParents/ChildrenAndParents/ChildClass.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ChildrenAndParents
+{
+    class Child
+    {
+        public string Name { get; private set; }
+        public double Age { get; private set; }
+        public Parent Father { get; private set; }
+        public Parent Mother { get; private set; }
+        public Child(string Name, double Age, Parent Father, Parent Mother)
+        {
+            if (Name is null) throw new Exception("Invalid value for Name field");
+            if (Age < 0) throw new Exception("Invalid value for Age field");
+            if (Father is null) throw new Exception("Invalid value for Father field");
+            if (Mother is null) throw new Exception("Invalid value for Mother field");
+            this.Name = Name;
+            this.Age = Age;
+            this.Father = Father;
+            this.Mother = Mother;
+        }
+    }
+}
---
+++ ChildrenAndParents/ChildrenAndParents/ParentClass.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ChildrenAndParents
+{
+    class Parent
+    {
+        public string Name { get; private set; }
+        public double Age { get; private set; }
+        public double Salary { get; private set; }
+        public Parent(string Name, double Age, double Salary)
+        {
+            if (Age < 0) throw new Exception("Invalid value for Age field");
+            if (Name is null) throw new Exception("Invalid value for Name field");
+            if (Salary < 0) throw new Exception("Invalid value for Salary field");
+            this.Name = Name;
+            this.Age = Age;
+            this.Salary = Salary;
+        }
+    }
+}
