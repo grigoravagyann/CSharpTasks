6a27147818b72636bf4a32e83b1a537ac05c34b7
Grigor Avagyan <grigor.avagyan01@gmail.com>
2025-03-18 17:32:53 +0400

    .

---
+++ MyString/MyString/MyStringClass.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace MyStringNamespace
+{
+    public class MyString
+    {
+        char[] _myStr;
+        public string String { get => new string(_myStr); }
+        public MyString()
+        {
+            _myStr = default;
+        }
+        public MyString(string str)
+        {
+            _myStr = str.ToCharArray();
+        }
+        public MyString(char[] arr)
+        {
+            _myStr = arr;
+        }
+        public MyString(string[] arr)
+        {
+            _myStr = string.Join("", arr).ToCharArray();
+        }
+        public bool Compare(MyString str)
+        {
+            if (str._myStr.Length != _myStr.Length)
+                return false;
+            for (int i = 0; i < _myStr.Length; i++)
+            {
+                if (_myStr[i] != str._myStr[i])
+                    return false;
+            }
+            return true;
+        }
+        public static bool operator ==(MyString str1, MyString str2)
+        {
+            if (str1 is null || str2 is null)
+                return false;
+            if (str1._myStr.Length != str2._myStr.Length)
+                return false;
+            for (int i = 0; i < str1._myStr.Length; i++)
+            {
+                if (str1._myStr[i] != str2._myStr[i])
+                    return false;
+            }
+            return true;
+        }
+        public static bool operator !=(MyString str1, MyString str2)
+        {
+            if (str1 is null || str2 is null)
+                return true;
+            if (str1._myStr.Length != str2._myStr.Length)
+                return true;
+            for (int i = 0; i < str1._myStr.Length; i++)
+            {
+                if (str1._myStr[i] != str2._myStr[i])
+                    return true;
+            }
+            return false;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is MyString)
+            {
+                MyString str = (MyString)obj;
+                if (new string(this._myStr) == new string(str._myStr))
+                    return true;
+            }
+            return false;
+        }
+        new public static bool Equals(object obj1, object obj2)
+        {
+            if (obj1 is MyString && obj2 is MyString)
+            {
+                MyString str1 = (MyString)obj1;
+                MyString str2 = (MyString)obj2;
+                if (new string(str1._myStr) == new string(str2._myStr))
+                    return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return _myStr.GetHashCode();
+        }
+        public static string Join(char s, MyString[] strs)
+        {
+            string retstr = "";
+            foreach (var str in strs)
+            {
+                if (str is null)
+                    return "Null";
+            }
+            foreach (var str in strs)
+            {
+                retstr = (retstr + s + new string(str._myStr));
+            }
+            return retstr.Substring(1);
+        }
+        public bool EndsWith(string substr)
+        {
+            if (new string(this._myStr).EndsWith(substr))
+                return true;
+            return false;
+
+        }
+        public bool StartsWith(string substr)
+        {
+            if (new string(this._myStr).StartsWith(substr))
+                return true;
+            return false;
+        }
+        public char this[int index]
+        {
+            get => _myStr[index];
+        }
+        public static MyString operator +(MyString str1, MyString str2)
+        {
+            return new MyString(new string(str1._myStr) + new string(str2._myStr));
+        }
+        public static MyString operator +(MyString str1, string str2)
+        {
+            return new MyString(new string(str1._myStr) + str2);
+        }
+    }
+
+}
--- MyString/MyString/Program.cs
+++ MyString/MyString/Program.cs
@@ -1,127 +1,32 @@
-﻿using System;
+﻿/*
+The task is to implement a custom string-like class, MyString, in C#. 
+Functional Requirements:
+    1) Constructors:
+        Implement multiple constructors for initializing MyString objects.
+        Constructor with no parameters (default constructor).
+        Constructor accepting a single string.
+        Constructor accepting a single char array.
+        Constructor that accepts a string array.
+    2) Comparison:
+        Implement Compare methods to compare the current MyString object with another string or MyString object.
+        Overload the == and != operators for equality and inequality comparison.
+    3) Equals:
+        Implement Equals(Object? other).
+        Implement a static version of Equals.
+    4) Join:
+        Implement Join methods that combine multiple MyString objects (or an array of MyString) into a single string, using a separator string.
+    5) EndsWith, StartsWith:
+        Implement an EndsWith method that checks if the MyString ends with a given substring.
+        Implement a StartsWith method that checks if the MyString starts with a given substring.
+    6) Indexer:
+        Implement an indexer to access individual characters in the MyString object by index (similar to the built-in string indexer).
+    7) Operator+:
+        Overload the + operator to allow concatenation of two MyString objects or a MyString object with a regular string.
 
-class MyString
-{
-    char[] _myStr;
-    public string String { get => new string(_myStr); }
-    public MyString()
-    {
-        _myStr = default;
-    }
-    public MyString(string str)
-    {
-        _myStr = str.ToCharArray();
-    }
-    public MyString(char[] arr)
-    {
-        _myStr = arr;
-    }
-    public MyString(string[] arr)
-    {
-        _myStr = string.Join("", arr).ToCharArray();
-    }
-    public bool Compare(MyString str)
-    {
-        if (str._myStr.Length != _myStr.Length)
-            return false;
-        for (int i = 0; i < _myStr.Length; i++)
-        {
-            if (_myStr[i] != str._myStr[i])
-                return false;
-        }
-        return true;
-    }
-    public static bool operator ==(MyString str1, MyString str2)
-    {
-        if(str1 is null || str2 is null)
-            return false;
-        if ( str1._myStr.Length != str2._myStr.Length)
-            return false;
-        for (int i = 0; i < str1._myStr.Length; i++)
-        {
-            if (str1._myStr[i] != str2._myStr[i])
-                return false;
-        }
-        return true;
-    }
-    public static bool operator !=(MyString str1, MyString str2)
-    {
-        if (str1 is null || str2 is null)
-            return true;
-        if (str1._myStr.Length != str2._myStr.Length)
-            return true;
-        for (int i = 0; i < str1._myStr.Length; i++)
-        {
-            if (str1._myStr[i] != str2._myStr[i])
-                return true;
-        }
-        return false;
-    }
-    public override bool Equals(object obj)
-    {
-        if(obj is MyString)
-        {
-            MyString str = (MyString)obj;
-            if (new string(this._myStr) == new string(str._myStr))
-                return true;
-        }
-        return false;
-    }
-    new public static bool Equals(object obj1,object obj2)
-    {
-        if(obj1 is MyString && obj2 is MyString)
-        {
-            MyString str1 = (MyString)obj1;
-            MyString str2 = (MyString)obj2;
-            if (new string(str1._myStr) == new string(str2._myStr))
-                return true;
-        }
-        return false;
-    }
-    public override int GetHashCode()
-    {
-        return _myStr.GetHashCode();
-    }
-    public static string Join(char s, MyString[] strs)
-    {
-        string retstr = "";
-        foreach (var str in strs)
-        {
-            if(str is null)
-                return "Null";
-        }
-        foreach (var str in strs)
-        {
-            retstr = (retstr + s + new string(str._myStr));
-        }
-        return retstr.Substring(1);
-    }
-    public bool EndsWith(string substr)
-    {
-        if(new string(this._myStr).EndsWith(substr))
-            return true;
-        return false;
+*/
 
-    }
-    public bool StartsWith(string substr)
-    {
-        if (new string(this._myStr).StartsWith(substr))
-            return true;
-        return false;
-    }
-    public char this[int index]
-    {
-        get => _myStr[index];
-    }
-    public static MyString operator +(MyString str1, MyString str2)
-    {
-        return new MyString(new string(str1._myStr) + new string(str2._myStr));
-    }
-    public static MyString operator +(MyString str1, string str2)
-    {
-        return new MyString(new string(str1._myStr) + str2);
-    }
-}
+using System;
+using MyStringNamespace;
 
 class Program
 {
@@ -131,6 +36,7 @@ class Program
         MyString str2 = new MyString("Hello World!");
         MyString[] strs = new MyString[2] { str1, str2 };
         string str3 = "Hello World!";
+
         Console.WriteLine($"String: {str1.String}");
         Console.WriteLine($"Compare : {str1.Compare(str2)}");
         Console.WriteLine($"Operator == : {str1 == str2}");
@@ -143,6 +49,7 @@ class Program
         Console.WriteLine($"Indexer: {str1[0]}");
         Console.WriteLine($"Concatenation: {(str1 + str2).String}");
         Console.WriteLine($"Concatenation with string: {(str1 + str3).String}");
+
         Console.ReadKey();
     }
 }
